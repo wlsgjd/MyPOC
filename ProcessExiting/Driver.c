db69edd693b942b7720d8f2d2ad49ab7f6602483
Jinhyeong Choe <cshelldll@naver.com>
2023-11-10 18:36:52 +0900

    Update Driver.c

--- ProcessExiting/Driver.c
+++ ProcessExiting/Driver.c
@@ -1,9 +1,49 @@
 #include "utils/utils.h"
 
-void SetProcessExiting(PEPROCESS eprocess, BOOL bExiting)
+void SetProcessExiting(PEPROCESS eprocess, BOOL bExiting, DWORD ExitStatus)
 {
+	/*
+	1: kd> dt_eprocess
+	ntdll!_EPROCESS
+	   ++0x464 Flags            : Uint4B
+	   +0x464 CreateReported   : Pos 0, 1 Bit
+	   +0x464 NoDebugInherit   : Pos 1, 1 Bit
+	   +0x464 ProcessExiting   : Pos 2, 1 Bit
+	   +0x464 ProcessDelete    : Pos 3, 1 Bit
+	   +0x464 ManageExecutableMemoryWrites : Pos 4, 1 Bit
+	   +0x464 VmDeleted        : Pos 5, 1 Bit
+	   +0x464 OutswapEnabled   : Pos 6, 1 Bit
+	   +0x464 Outswapped       : Pos 7, 1 Bit
+	   +0x464 FailFastOnCommitFail : Pos 8, 1 Bit
+	   +0x464 Wow64VaSpace4Gb  : Pos 9, 1 Bit
+	   +0x464 AddressSpaceInitialized : Pos 10, 2 Bits
+	   +0x464 SetTimerResolution : Pos 12, 1 Bit
+	   +0x464 BreakOnTermination : Pos 13, 1 Bit
+	   +0x464 DeprioritizeViews : Pos 14, 1 Bit
+	   +0x464 WriteWatch       : Pos 15, 1 Bit
+	   +0x464 ProcessInSession : Pos 16, 1 Bit
+	   +0x464 OverrideAddressSpace : Pos 17, 1 Bit
+	   +0x464 HasAddressSpace  : Pos 18, 1 Bit
+	   +0x464 LaunchPrefetched : Pos 19, 1 Bit
+	   +0x464 Background       : Pos 20, 1 Bit
+	   +0x464 VmTopDown        : Pos 21, 1 Bit
+	   +0x464 ImageNotifyDone  : Pos 22, 1 Bit
+	   +0x464 PdeUpdateNeeded  : Pos 23, 1 Bit
+	   +0x464 VdmAllowed       : Pos 24, 1 Bit
+	   +0x464 ProcessRundown   : Pos 25, 1 Bit
+	   +0x464 ProcessInserted  : Pos 26, 1 Bit
+	   +0x464 DefaultIoPriority : Pos 27, 3 Bits
+	   +0x464 ProcessSelfDelete : Pos 30, 1 Bit
+	   +0x464 SetTimerResolutionLink : Pos 31, 1 Bit
+
+	   ...
+
+	   // +0x7d4 ExitStatus       : Int4B
+	*/
 	EprocessFlags*	Flags = (EprocessFlags*)((BYTE*)eprocess + 0x464);
 	EprocessFlags	org_flags = *Flags;
+
+	DWORD* pExitStatus = (DWORD*)((BYTE*)eprocess + 0x7D4);
 
 	if (bExiting)
 	{
@@ -12,6 +52,8 @@ void SetProcessExiting(PEPROCESS eprocess, BOOL bExiting)
 		Flags->ProcessInSession		= FALSE;
 		Flags->ProcessRundown		= TRUE;
 		Flags->ProcessSelfDelete	= TRUE;
+
+		*pExitStatus = ExitStatus;
 	}
 	else
 	{
@@ -39,7 +81,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPat)
 		DbgPrint("PID : %d\n", pid);
 		DbgPrint("EPROCESS : 0x%p\n", eprocess);
 
-		SetProcessExiting(eprocess, TRUE);
+		SetProcessExiting(eprocess, TRUE, 0);
 	}
 	return STATUS_UNSUCCESSFUL;
 }
