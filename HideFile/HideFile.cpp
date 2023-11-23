d080b7bac51f2bf82e1de72c18dbbc4f56d53ca5
Jinhyeong Choe <cshelldll@naver.com>
2023-11-23 16:21:36 +0900

    HideFile

---
+++ HideFile/HideFile.cpp
@@ -0,0 +1,20 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+using namespace std;
+
+// HideFile.exe [Path]
+void _tmain(int argc, const TCHAR* argv[])
+{
+	if (argc != 2)
+	{
+		_tprintf(_T("argument error.\n"));
+		return;
+	}
+
+    SetFileAttributes(argv[1], FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM);
+
+    printf("success hide.");
+    system("pause");
+}
